e5ea7bdddabe7b5e2cbf23a07b83e2bef61cf69b
npadovani <normant_padovani@hotmail.com>
2022-06-08 20:53:18 +0000

    print the alphabet in lower and upper case

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alfabeto;
+	for (alfabeto = 'a'; alfabeto <= 'z'; alfabeto++)
+	{
+		putchar(alfabeto);
+	}
+	for (alfabeto = 'A'; alfabeto <= 'Z'; alfabeto++)
+	{
+		putchar(alfabeto);
+	}
+
+
+		putchar('\n');
+	return (0);
+}
+
