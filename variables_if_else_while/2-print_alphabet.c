1586fd61c9351370daff797c39f298b0b447bed3
npadovani <normant_padovani@hotmail.com>
2022-06-08 20:42:54 +0000

    print the alphabet and newline afterwards, after the loop

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alfabeto;
+	for (alfabeto = 'a'; alfabeto <= 'z'; alfabeto++)
+	{
+		putchar(alfabeto);
+	}
+
+		putchar('\n');
+	return (0);
+}
