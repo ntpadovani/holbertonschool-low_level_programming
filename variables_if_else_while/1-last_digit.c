35353efd5915a38fcdd77fe23947cf46676cb136
npadovani <normant_padovani@hotmail.com>
2022-06-08 19:58:27 +0000

    random variable values are compared within the last digit and as a result there is a message shown to the user

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int ldigt;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is", n); /* your code goes there */
+		ldigt = n % 10;
+		if (ldigt > 5)
+		{
+			printf(" %d and is greater than 5", ldigt);
+		}
+		else if (ldigt == 0)
+		{
+			printf(" %d and is 0", ldigt);
+		}
+		else if ((ldigt < 6) && (ldigt != 0))
+		{
+			printf(" %d and is less than 6 and not 0", ldigt);
+		}
+		else
+
+	return (0);
+}
