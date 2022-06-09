ea003c5120bff289a3e96d16b62031d47c23185a
npadovani <normant_padovani@hotmail.com>
2022-06-09 14:26:15 +0000

    Prints the alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alfabeto;
+	for (alfabeto = 'z'; alfabeto >= 'a'; alfabeto--)
+	{
+		putchar(alfabeto);
+	}
+		putchar('\n');
+	return (0);
+}
+
