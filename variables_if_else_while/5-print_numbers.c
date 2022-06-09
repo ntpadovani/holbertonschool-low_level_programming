9bfb245f170667fce3d3d8fb045dfc9b00af851c
npadovani <normant_padovani@hotmail.com>
2022-06-09 13:38:42 +0000

    a code that will print all base 10 numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+	for (number = '0'; number < '10'; number++)
+	{
+		printf("%d", number);
+
+	}
+	printf("\n");
+	return (0);
+}
