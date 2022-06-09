a3415a18f287d60172a276e2c690cb839dcb5920
npadovani <normant_padovani@hotmail.com>
2022-06-09 15:06:24 +0000

    A code that will print all possible single digit combinations with a comma and a space in between

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,9 +11,14 @@ int number;
 	for (number = 0; number < 10; number++)
 	{
 		putchar((number % 10) + '0');
+			if (number == 9)
+			{
+				goto nocomma;
+			}
 		putchar(',');
 		putchar(' ');
 	}
+	nocomma:
 	putchar('\n');
 	return (0);
 }
