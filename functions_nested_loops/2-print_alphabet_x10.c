c70bf391233be8b4c3e64bce3e76253a9b7d2991
npadovani <normant_padovani@hotmail.com>
2022-06-10 01:59:35 +0000

    A code that will display the alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char alfabeto;
+int count = 0;
+	while (count <= 10)
+	{
+		for (alfabeto = 'a'; alfabeto <= 'z'; alfabeto++)
+		{
+			_putchar(alfabeto);
+		}
+		_putchar('\n');
+		count++;
+	}
+}
+
