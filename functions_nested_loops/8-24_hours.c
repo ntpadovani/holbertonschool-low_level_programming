75fe1206e52a2a68197c65659f49ae15e0be3356
npadovani <normant_padovani@hotmail.com>
2022-06-10 22:18:22 +0000

    A code to display the 24 hours of the day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Displays the hours & minutes of the day
+ *
+ */
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -11,4 +11,5 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
 /*#endif*/
