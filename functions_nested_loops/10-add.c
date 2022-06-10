508aa4d1e40e2dcb79dd5b0cb5f019ecd0bc13e0
npadovani <normant_padovani@hotmail.com>
2022-06-10 22:43:09 +0000

    A code to add 2 numbers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - A code to add two integers
+ * @n1: First variable integer
+ * @n2: Second variable integer
+ *
+ * Return: The total
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
