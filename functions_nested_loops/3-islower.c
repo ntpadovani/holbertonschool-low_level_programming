739a06fbb9a84cf65b50426bc94d86a323a5bda3
npadovani <normant_padovani@hotmail.com>
2022-06-10 19:44:05 +0000

    A code that will evaluate if a letter is lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * _islower - Entry point
+ * @c: Argument
+ * Return: (1) is good
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
