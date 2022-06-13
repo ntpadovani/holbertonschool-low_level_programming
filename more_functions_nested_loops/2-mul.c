24fba15d0070b81ebce5a41384bbffcf574622dc
npadovani <normant_padovani@hotmail.com>
2022-06-13 15:53:47 +0000

    A code that will ultiply 2 numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul-Multiplies two integers
+ *@a: The first number to be multiplied
+ *@b: The second number to be multiplied
+ * Return: The result of the multiplication
+ */
+int mul(int a, int b)
+{
+int result;
+	result = a * b;
+	return (result);
+}
