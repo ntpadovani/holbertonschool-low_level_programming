1c1a79b6bd030f8a4439d5c6a0639e73003c212a
npadovani <normant_padovani@hotmail.com>
2022-06-14 15:40:37 +0000

    A code that will print a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_triangle-Draws a triangle in the terminal
+ *@size: The size of the triangle to be printed
+ * Return: Void
+ */
+void print_triangle(int size)
+{
+
+int side1;
+int side2;
+int line;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (side1 = 1; side1 <= size; side1++)
+	{
+		for (side2 = 1; side2 <= (size - side1); side2++)
+		{
+			_putchar(' ');
+		}
+		for (line = 1; line <= side1; line++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
