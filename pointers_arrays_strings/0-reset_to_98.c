7090cf12476c372eb04282eda558cc097d16b7ae
npadovani <normant_padovani@hotmail.com>
2022-06-15 00:13:15 +0000

    A code that will reset the value of pointer n to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 -Takes a pointer to an int as parameter
+ *and updates the value it points to to 98.
+ *@n: Holds the value to be input for the funtion.
+ * Return: Void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;*n = 98;*n = 98;*n = 98;*n = 98;*n = 98;*n = 98;*n = 98;
+}
