0eff08e96ba2a82adb0d8ccb1d072f99fbd6aab6
npadovani <normant_padovani@hotmail.com>
2022-06-15 01:47:15 +0000

    A code that will swap 2 pointer values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int -swaps the values of two
+ *integers.
+ *@a: Holds 1 of the values to be swapped.
+ *@b: Holds the other value to be swapped.
+ * Return: Void
+ */
+void swap_int(int *a, int *b)
+{
+int swap;
+swap = *b;
+*b = *a;
+printf("swap is %d\n", swap);
+*a = swap;
+}
