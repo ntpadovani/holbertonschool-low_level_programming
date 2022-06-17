3e8cb13a78ebf00fcea03d2c4e83774a104a659c
npadovani <normant_padovani@hotmail.com>
2022-06-17 18:42:24 +0000

    A code that will copy one string to another

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,29 @@
 #include "main.h"
 
 /**
- * _strcpy-Copy a string
- *@dest: one of the strings to be copied
- *@src: The source of the string to be copied
- * Return: the pointer to destination string
+ *_strlen -a function that returns the length of a string.
+ *@s: The string
+ * Return: The lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+
+	return (lenght);
+}
+
+/**
+* _strcpy-Copy a string
+*@dest: one of the strings to be copied
+*@src: The source of the string to be copied
+* Return: the pointer to destination string
 */
 
 char *_strcpy(char *dest, char *src)
