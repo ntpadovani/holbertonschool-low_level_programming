fc077b74a27c2f4d646eae76111ee0298fd59da5
npadovani <normant_padovani@hotmail.com>
2022-06-18 20:19:14 +0000

    updated for loop

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,16 +12,16 @@ void print_array(int *a, int n)
 int idx;
 	for (idx = 0; idx < n; idx++)
 	{
-		if (idx != (n - 1))
+		if (idx == (n - 1))
 		{
 			printf("%d", a[n]);
 		}
-	else
-	{
-		printf("%d", a[n]);
-		printf(",");
-		printf(" ");
-	}
+		else
+		{	
+			printf("%d", a[n]);
+			printf(",");
+			printf(" ");
+		}
 	}
 	printf("\n");
 }
