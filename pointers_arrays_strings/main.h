fab016e92d80434b8171ff0333231c7e0fcc8df5
npadovani <normant_padovani@hotmail.com>
2022-06-22 20:20:59 +0000

    A code that will copy a string

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -45,5 +45,5 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 char *_strncat(char *dest, char *src, int n);
-
+char *_strncpy(char *dest, char *src, int n);
 /*#endif*/
