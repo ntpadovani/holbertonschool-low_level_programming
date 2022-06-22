ed9e8ba0d357c1a2af928131fc4c260ba3a23af1
npadovani <normant_padovani@hotmail.com>
2022-06-22 19:03:21 +0000

    Putchar

--- static_libraries/_putchar.c
+++ static_libraries/_putchar.c
@@ -1,11 +1,14 @@
-#include"main.h"
+#include <unistd.h>
+
 /**
- * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	printf("_putchar\n");
-return (0);
+	return (write(1, &c, 1));
 }
+
