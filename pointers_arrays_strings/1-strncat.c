0b26b7486fedc4539be8567cc60345a3add3dc49
npadovani <normant_padovani@hotmail.com>
2022-06-22 20:15:13 +0000

    A function that will append the contents of a string to another

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+/**
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int lenght = 0, idx = 0;
+
+	while (*(dest + lenght) != '\0')
+	{
+		lenght++;
+	}
+
+	while (idx < n)
+	{
+		*(dest + lenght) = *(src + idx);
+		if (*(src + idx) == '\0')
+		{
+			break;
+		}
+		lenght++;
+		idx++;
+	}
+	return (dest);
+}
