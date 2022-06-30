bf26c7592be9bd0ee40757eb9d1b2d46a9e8438e
npadovani <normant_padovani@hotmail.com>
2022-06-30 19:31:14 +0000

    A program that prints the name of the file it was compiled from, followed by a new line.

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main -A program that prints the name of
+* the it was compiled from.
+*
+* Return: The name of the file.
+*/
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
