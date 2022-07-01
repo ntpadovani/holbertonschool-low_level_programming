99c121fb944f684684919015160d441dbab75a0c
npadovani <normant_padovani@hotmail.com>
2022-07-01 15:05:53 +0000

    A function that concatenates all the arguments of your program.

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -30,4 +30,10 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *argstostr(int ac, char **av);
 #endif
