459e105abeea78ec6fc054d59ca4e2f551db00f0
npadovani <normant_padovani@hotmail.com>
2022-07-01 15:19:03 +0000

    A function that splits a string into words.

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -36,4 +36,5 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 #endif
