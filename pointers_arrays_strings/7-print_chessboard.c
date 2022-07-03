e566046c0b0ee9db520171ece7a3f7c31ce6871b
npadovani <normant_padovani@hotmail.com>
2022-07-03 21:20:56 +0000

    A function that prints a type of chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints a chessboard.
+ *
+ * @a: Matrix.
+ * Return: should be 0.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar(10);
+	}
+}
