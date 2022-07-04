e6e5268e0e41d898f4890183b778369bd48ac884
npadovani <normant_padovani@hotmail.com>
2022-07-04 16:41:22 +0000

    A struct named dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,8 +3,18 @@
 
 #include "main.h"
 
+/**
+* struct dog - A struc called dog
+*
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The owner of the dog.
+* Description: A struct used to specify a dog's name,
+*              age and owner's name.
+*/
 
-struct dog{
+struct dog
+{
 char *name;
 float age;
 char *owner;
