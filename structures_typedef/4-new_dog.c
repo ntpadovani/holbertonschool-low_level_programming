3a27900a2bbc6ac6def8664dd2bb3d0a31c18a4d
npadovani <normant_padovani@hotmail.com>
2022-07-05 01:17:21 +0000

    A added the functions _strlen and _strcpy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,45 @@
 #include <stdlib.h>
 
 /**
+* _strcpy-Copy a string
+*@dest: one of the strings to be copied
+*@src: The source of the string to be copied
+* Return: the pointer to destination string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int idx;
+int lenght;
+
+	lenght = _strlen(src);
+	for (idx = 0; idx <= lenght; idx++)
+	{
+		dest[idx] = src[idx];
+	}
+return (dest);
+}
+
+
+/**
+ *_strlen -a function that returns the length of a string.
+ *@s: The string
+ * Return: The lenght of the string
+ */
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+
+	return (lenght);
+}
+	
+/**
 * new_dog - A funtion to create a new struct dog.
 *
 * @name: The string of type char that will store the
