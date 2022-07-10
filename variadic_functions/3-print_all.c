00f17cb31523076700f5ce2591d536d0f57190d0
npadovani <normant_padovani@hotmail.com>
2022-07-10 22:18:48 +0000

    A function that print anything.

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,107 @@
+#include "variadic_functions.h"
+
+/**
+* print_char - A function that prints a char.
+* @ptr:  Contains the values.
+* Return:  Void
+*/
+
+void print_char(va_list ptr)
+{
+	char storage;
+
+	storage = va_arg(ptr, int);
+	printf("%c", storage);
+}
+
+/**
+* print_int - A function that prints an integer.
+* @ptr:  Contains the values.
+* Return:  Void
+*/
+void print_int(va_list ptr)
+{
+	int storage;
+
+	storage = va_arg(ptr, int);
+	printf("%d", storage);
+}
+
+/**
+* print_float - A function that prints a decimal.
+* @ptr:  Contains the values.
+* Return:  Void
+*/
+void print_float(va_list ptr)
+{
+	double storage;
+
+	storage = va_arg(ptr, double);
+	printf("%f", storage);
+}
+
+/**
+* print_string - A function that prints a string array.
+* @ptr:  Contains the values.
+* Return:  Void
+*/
+void print_string(va_list ptr)
+{
+	char *storage;
+
+	storage = va_arg(ptr, char *);
+	if (storage == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", storage);
+}
+
+/**
+* print_all - A function that prints anything.
+*
+* @format:  Contains the list.
+*
+* Return:  Void
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list ptr;
+	int idx = 0;
+	int idx2 = 0;
+
+	escoge_t option[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string}
+	};
+
+	va_start(ptr, format);
+
+
+	while (format[idx] != '\0')
+	{
+		idx2 = 0;
+		while (idx2 < 4)
+		{
+
+			if (format[idx] == option[idx2].data)
+			{
+				option[idx2].f(ptr);
+				if (format[idx + 1] != '\0')
+				{
+					printf(", ");
+				}
+			}
+			idx2++;
+		}
+		idx++;
+	}
+	printf("\n");
+	va_end(ptr);
+}
+
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,9 +5,28 @@
 #include<stdlib.h>
 #include<stdarg.h>
 
+/*Defined type structs*/
+
+/**
+* type - A struct function.
+* 
+* @data: The variable containing the 
+*        left side letter in our
+*        2d array structure
+* @f:    It points to the function on the right
+*        side of  our 2d array structure.
+*/
+typedef struct type
+{
+        char data;
+        void (*f)(va_list ptr);
+} escoge_t;
+
+
 /*From here below, the funtion prototypes*/
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
