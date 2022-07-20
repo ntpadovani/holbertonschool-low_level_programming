62af86d81da8eb8b489b552a2e183cd9958b51a8
npadovani <normant_padovani@hotmail.com>
2022-07-20 13:20:29 +0000

    A function that prints a list from a list.

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,37 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
+
+/*Libraries used for the project*/
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
+/*
+* All singly list project prototypes
+* from here on below
+*/
+
+size_t print_list(const list_t *h);
+
+
+
+
+#endif
