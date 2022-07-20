4b34355c3a83252ea74a1c2eae3032ad1e95a067
npadovani <normant_padovani@hotmail.com>
2022-07-20 19:25:18 +0000

    Updates the code to remove the reference to my _strlen function on a separete file to be included in the add_node file code

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,26 @@
 #include"lists.h"
 
+#include "lists.h"
+
+/**
+ * _strlen -  function that returns the length of a string
+ * @s: ...
+ * Return: returns the length of a string
+ */
+
+int _strlen(const char *s) 
+{
+        int idx = 0;
+
+        while (*s != '\0')
+        {
+                idx++;
+                s++;
+        }
+        return (idx);
+}
+
+
 /**
 * add_node - A function that adds a new node
 * at the beggining of a list.
