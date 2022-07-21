d70f0849ed21281b29b7bd75733e7e128637fd49
npadovani <normant_padovani@hotmail.com>
2022-07-21 04:08:40 +0000

    Fixed error with struct declaration with bracket.

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -32,7 +32,8 @@ typedef struct list_s
  * Description: singly linked list node structure
  *
  */
-typedef struct listint_s{
+typedef struct listint_s
+{
 int n;
 struct listint_s *next;
 } listint_t;
