7cc1f66b260d2ad15c8e009dfefd14ca805bdb59
npadovani <normant_padovani@hotmail.com>
2022-07-21 03:35:21 +0000

    A function that prints a message before the main function is executed.

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void print_something(void) __attribute__ ((constructor));
+
+/**
+ *print_something - prints a msg before the main
+ */
+
+void print_something(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
